d8d0d1631fa9d9a543de5038a3b3c7cd89892bed
jbachlombardo <jbachlombardo@gmail.com>
2020-02-16 23:54:06 +0100

    Update clock.py

--- clock.py
+++ clock.py
@@ -6,6 +6,6 @@ scheduler = BlockingScheduler()
 def elo_job() :
     runpy.run_path('chessead_elo_sheets.py')
 
-scheduler.add_job(elo_job, 'interval', weeks = 1, start_date='2020-02-15 12:00:00')
+scheduler.add_job(elo_job, 'interval', weeks = 1, start_date='2020-02-16 12:00:00')
 
 scheduler.start()
