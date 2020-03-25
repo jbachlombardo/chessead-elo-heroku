ac9c5becb77bb6266ecf20b6d7fc4c8b3b3f2c4e
jbachlombardo <jbachlombardo@gmail.com>
2020-03-25 11:13:49 +0100

    updated run 2x weekly wed sun

--- clock.py
+++ clock.py
@@ -6,6 +6,7 @@ scheduler = BlockingScheduler()
 def elo_job() :
     runpy.run_path('chessead_elo_sheets.py')
 
-scheduler.add_job(elo_job, 'interval', weeks = 1, start_date='2020-02-16 12:00:00')
+# scheduler.add_job(elo_job, 'interval', weeks = 1, start_date='2020-02-16 12:00:00')
+scheduler.add_job(elo_job, 'cron', day_of_week = 'wed, sun', hour = 12)
 
 scheduler.start()
